8ca5e81100cfd47e0f32600ef241e137c33645e4
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-19 20:15:20 +0100

    task

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+""" a Python script that, using this REST API"""
+
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+
+    response = requests.get(base_url + "users/" + employee_id)
+    employee_name = response.json()["username"]
+
+    # Get the employee's TODO list from the API
+    response = requests.get(base_url + "todos?userId=" + employee_id)
+    todo_list = response.json()
+
+    # Create a list of dictionaries to store the tasks
+    tasks = []
+
+    # Loop through the TODO list
+    for task in todo_list:
+        # Create a dictionary for each task
+        task_dict = {}
+        # Store the relevant information in the dictionary
+        task_dict["task"] = task["title"]
+        task_dict["completed"] = task["completed"]
+        task_dict["username"] = employee_name
+        # Append the dictionary to the list
+        tasks.append(task_dict)
+
+    # Create a dictionary with the user id as the key & the tasks as the value
+    data = {employee_id: tasks}
+
+    # Create a JSON file named USER_ID.json
+    with open(employee_id + ".json", "w") as jsonfile:
+        # Write the data to the file with 4 spaces indentation
+        json.dump(data, jsonfile)
