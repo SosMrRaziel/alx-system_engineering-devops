ae3ebd132735a9bdb47fab6ee0f14298e9fe1939
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-03-05 18:06:05 +0100

    task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers for a given subreddit"""
+    # Set the custom user-agent
+    headers = {'User-Agent': 'ALX:subscribers_counter:v1.0 (by /u/SosMrRaziel)'}
+    # Make a GET request to the subreddit's about.json endpoint
+    response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
+    # Check the status code and the json data
+    if response.status_code == 200 and response.json().get('kind') == 't5':
+        # Return the subscriber count
+        return response.json().get('data').get('subscribers')
+    else:
+        # Return 0 for invalid subreddit
+        return 0
