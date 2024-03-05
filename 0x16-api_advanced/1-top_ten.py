f92d34c350e2a3f078a4c3a87f9db952406c0b38
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-03-05 19:41:26 +0100

    task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-""" 
+"""
     a function that queries the Reddit API and prints
     the titles of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
 
+
 def top_ten(subreddit):
-  # Set the base URL and the headers for the API request
-  base_url = "https://www.reddit.com/"
-  headers = {"User-Agent": "ALX:subscribers_counter:v1.0 (by /u/SosMrRaziel)"}
+    # Set the base URL and the headers for the API request
+    base_url = "https://www.reddit.com/"
+    headers = {
+        "User-Agent": "ALX:subscribers_counter:v1.0 (by /u/SosMrRaziel)"}
 
-  # Construct the full URL for the subreddit's hot listing
-  url = base_url + "r/" + subreddit + "/hot.json"
+    # Construct the full URL for the subreddit's hot listing
+    url = base_url + "r/" + subreddit + "/hot.json"
 
-  # Send a GET request to the API and get the response
-  response = requests.get(url, headers=headers, allow_redirects=False)
+    # Send a GET request to the API and get the response
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-  # Check if the response status code is 200 (OK) and the subreddit exists
-  if response.status_code == 200:
-    # Get the first 10 posts from the response data
-    posts = response.json()["data"]["children"][:10]
-
-    # Print the titles of the posts
-    for post in posts:
-      print(post["data"]["title"])
-  else:
-    # Print None if the subreddit is invalid or empty
-    print(None)
+    # Check if the response status code is 200 (OK) and the subreddit exists
+    if response.status_code == 200:
+        # Get the first 10 posts from the response data
+        posts = response.json()["data"]["children"][:10]
+        # Print the titles of the posts
+        for post in posts:
+            print(post["data"]["title"])
+    else:
+        # Print None if the subreddit is invalid or empty
+        print(None)
