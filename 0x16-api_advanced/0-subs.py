e79366822480fcfc95186280418be29fd9de327f
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-03-05 18:46:02 +0100

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,9 +13,9 @@ def number_of_subscribers(subreddit):
         'https://www.reddit.com/r/{}/about.json'.format(
             subreddit), headers=headers)
     # Check the status code and the json data
-    if response.status_code == 200 and response.json().get('kind') == 't5':
-        # Return the subscriber count
-        return response.json().get('data').get('subscribers')
-    else:
+    if response.status_code != 200:
         # Return 0 for invalid subreddit
         return 0
+    else:
+        # Return the subscriber count
+        return response.json().get('data').get('subscribers')
