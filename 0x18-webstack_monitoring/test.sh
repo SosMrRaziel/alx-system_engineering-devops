0466ad7c563b3c0f9c5f4f6eb5b8ab89b76004ca
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-03-06 23:11:54 +0100

    task

---
+++ 0x18-webstack_monitoring/test.sh
@@ -0,0 +1,4 @@
+curl -X GET "https://api.datadoghq.com/api/v1/hosts" \
+-H "Accept: application/json" \
+-H "DD-API-KEY: a5e53d4501bb5e0358e11bc1594f0958" \
+-H "DD-APPLICATION-KEY: baf4931045a27b24f31977e2ea744c83aa5025c1"
